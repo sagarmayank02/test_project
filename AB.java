4ec1683009543693644106aff1a0c9e1d426a276
sagarmayank02 <110383833+sagarmayank02@users.noreply.github.com>
2022-08-01 14:26:09 +0530

    Add files via upload

---
+++ AB.java
@@ -0,0 +1,29 @@
+package maggi2;
+
+public class AB {
+
+		int a=5,b=3,c=4;
+		static int d=5,e=6;
+		public static void main(System args[]) {
+		AB ob=new AB();
+		System.out.println(ob.a+ob.b+ob.c+d+e);
+		ob.a=ob.b+d;
+		ob.b=ob.c+e;
+		d=ob.a+ob.b;
+		System.out.println(ob.a+ob.b+ob.c+d+e);
+		AB ob1=new AB();
+		ob1.a=ob.a+ob1.b;
+		ob1.b=ob1.a+ob1.c;
+		e=ob1.a+ob1.b;
+		System.out.println(ob1.a+ob1.b+ob1.c+d+e);
+		AB ob2=new AB();
+		System.out.println(ob2.a+ob2.b+ob2.c+d+e);
+		
+		
+	}}
+
+
+
+
+
+
